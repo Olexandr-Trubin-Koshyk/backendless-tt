51f21ae4c96f55aa1127066a9aec4147fd22bbac
Alexandr Trubin-Koshyk <alexandr.koshyk@gmail.com>
2022-05-01 14:07:32 +0300

    Changed HandleChange to HandlePressKey and HandleBlur

--- src/components/InputChartMenu/InputChartMenu.tsx
+++ src/components/InputChartMenu/InputChartMenu.tsx
@@ -7,21 +7,34 @@ interface Props {
 }
 
 export const InputChartMenu: React.FC<Props> = React.memo(({axis, value, setValue}) => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePressEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.currentTarget.value 
+      ? setValue(event.currentTarget.value.split(',')) 
+      : setValue([]);
+      
+      console.log(value)
+    } 
+  }
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
     event.target.value 
       ? setValue(event.target.value.split(',')) 
       : setValue([]);
+      console.log(value)
   }
+
     return (
       <div className="inputMenu">
         <p className="inputMenu__title">
           {`${axis} axis labels:`}
         </p>
         <input 
-        className={axis === 'X' ? 'inputMenu__months' : 'inputMenu__valuesByMonths'}
-        onChange={handleChange}
-        defaultValue={value.join(', ')}
-      />
+          className={axis === 'X' ? 'inputMenu__months' : 'inputMenu__valuesByMonths'}
+          onKeyPress={handlePressEnter}
+          onBlur={handleBlur}
+          defaultValue={value.join(', ')}
+        />
       </div>
     )
 })
